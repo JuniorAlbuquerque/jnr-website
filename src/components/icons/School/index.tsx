d19c944b390bd6efc4fc764c3c695087f75309af
Junior Albuquerque <junioralbuquerque81@gmail.com>
2022-07-05 21:16:47 -0400

    chore: add new icons

---
+++ src/components/icons/School/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const IconSchool: React.FC = () => {
+  return (
+    <svg
+      width="28"
+      height="28"
+      viewBox="0 0 28 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M20.125 19.1758L14 22.6211L7.875 19.1758V15.5357L6.125 14.5635V20.1992L14 24.6289L21.875 20.1992V14.5635L20.125 15.5357V19.1758V19.1758Z"
+        fill="currentColor"
+      />
+      <path
+        d="M14 2.5144L1.75 8.86625V10.383L14 17.1883L24.5 11.3552V16.1875H26.25V8.86625L14 2.5144ZM22.75 10.3255L21 11.2977L14 15.1868L7 11.2977L5.25 10.3255L4.03709 9.65161L14 4.48567L23.9629 9.65161L22.75 10.3255Z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
+
+export default IconSchool
