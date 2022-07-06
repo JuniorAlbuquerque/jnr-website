25ee0173f1e3bdfc1909d9b6c7065c6a877625eb
Junior Albuquerque <junioralbuquerque81@gmail.com>
2022-07-05 21:16:38 -0400

    chore: create card component

---
+++ src/components/Card/styles.ts
@@ -0,0 +1,10 @@
+import { css } from '@/styles/stiches.config'
+
+export const styles = {
+  root: css({
+    background: '$gray2',
+    padding: '$5',
+    width: '100%',
+    borderRadius: '$4'
+  })
+}
