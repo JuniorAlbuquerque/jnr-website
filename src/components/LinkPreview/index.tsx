440d5fef78da614970d27d2b4c511eafa077c5c3
Junior Albuquerque <junioralbuquerque81@gmail.com>
2022-07-13 23:53:43 -0400

    fix: get preview link data

--- src/components/LinkPreview/index.tsx
+++ src/components/LinkPreview/index.tsx
@@ -3,7 +3,6 @@ import { FC, Fragment, useEffect, useState } from 'react'
 import IconLink from '../icons/Link'
 import Shimmer from '../Shimmer'
 import { styles } from './styles'
-import { getLinkData } from 'link-preview-tags'
 
 type LinkPreviewProps = {
   url: string
@@ -44,10 +43,9 @@ const LinkPreview: FC<LinkPreviewProps> = ({
 
   const scrap = async () => {
     setIsLoading(true)
-    const data = await getLinkData(url)
-    // const { data } = await axios.get(
-    //   `https://link-preview-ts.vercel.app/api/v1/scrap?url=${url}`
-    // )
+    const { data } = await axios.get(
+      `https://link-preview-ts.vercel.app/api/v1/scrap?url=${url}`
+    )
     setIsLoading(false)
     setPreviewData(data)
   }
